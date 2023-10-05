93780a768ec2f556ce19df399f3cca35a21cf128
Bo <2529619267@qq.com>
2023-10-05 17:05:50 +0800

    完善登录页

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import 'element-plus/dist/index.css'
 import 'virtual:windi.css'
 import './style.css'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router/index'
 
 
+const app = createApp(App)
+app.use(ElementPlus).use(router).mount('#app')
 
-createApp(App).use(ElementPlus).use(router).mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -2,14 +2,9 @@
   <!-- <div>登录页面</div> -->
 
   <el-row style="min-height: 100vh">
-    <el-col
-      :span="16"
-      class="flex items-center justify-center bg-violet-400 flex-col"
-    >
+    <el-col :lg="16" :md="12" class="flex items-center justify-center bg-violet-400 flex-col">
       <div>
-        <div
-          class="grid-content ep-bg-purple text-left font-semibold text-5xl text-sky-50 mb-4"
-        >
+        <div class="grid-content ep-bg-purple text-left font-semibold text-5xl text-sky-50 mb-4">
           欢迎
         </div>
         <div class="grid-content ep-bg-purple text-cyan-50">
@@ -17,10 +12,7 @@
         </div>
       </div>
     </el-col>
-    <el-col
-      :span="8"
-      class="flex items-center justify-center bg-stone-50 flex-col"
-    >
+    <el-col :lg="8" :md="12" class="flex items-center justify-center bg-stone-50 flex-col">
       <div class="grid-content ep-bg-purple">
         <h2 class="text-5xl font-bold text-slate-500">开始浏览</h2>
         <div class="flex items-center justify-center my-5 space-x-2">
@@ -28,14 +20,24 @@
           <span class="">账号或密码登录</span>
           <span class="h-[1px] w-16 bg-gray-200"></span>
         </div>
-        <el-form :model="form"  class="w-[300px]">
+        <el-form :model="form" class="w-[300px]">
           <el-form-item label="用户名">
-            <el-input
-              v-model="form.username"
-              placeholder="请输入用户名"
-            /> </el-form-item
-          ><el-form-item label="密码">
-            <el-input v-model="form.password" placeholder="请输入密码" />
+            <el-input v-model="form.username" placeholder="请输入用户名">
+              <template #prefix>
+                <el-icon>
+                  <User />
+                </el-icon>
+              </template>
+            </el-input>
+
+          </el-form-item><el-form-item label="密码">
+            <el-input v-model="form.password" placeholder="请输入密码">
+              <template #prefix>
+                <el-icon>
+                  <Lock />
+                </el-icon>
+              </template>
+            </el-input>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" round @click="onSubmit" class="w-40 m-auto">登录</el-button>
@@ -61,7 +63,7 @@ const onSubmit = () => {
 </script>
 
 <style  scoped>
-/deep/ .el-form-item__label{
+/deep/ .el-form-item__label {
   width: 20%;
 }
 </style>
